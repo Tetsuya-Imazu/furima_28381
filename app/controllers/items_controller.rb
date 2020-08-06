90f0df275d5ab71a3799385e303aaa798e9ae23e
Tetsuya-Imazu <tty8131b@gmail.com>
2020-08-06 13:40:17 +0900

    商品出品機能実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,11 +1,13 @@
 class ItemsController < ApplicationController
+  # before_action :move_to_index, except: [:index, :show]
 
   def index
     @items = Item.all.order("created_at DESC")
   end
 
   def new
-    @items = Item.new
+    @item = Item.new
+    
   end
 
   def create
@@ -21,6 +23,13 @@ class ItemsController < ApplicationController
 
   def item_params
     params.require(:item).permit(:name, :description, :category_id, :condition_id, :cost_id, :prefecture_id, :shipping_id, :price)
+    # .merge(user_id: current_user.id)
+  end
+
+  def move_to_index
+    unless user_signed_in?
+      redirect_to new_user_registration_path
+    end
   end
 
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,14 +1,18 @@
 class Item < ApplicationRecord
-  belongs_to :user
-  has_one :shipping
-  has_one :item_purchase
-
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :category
+  belongs_to_active_hash :condition
+  belongs_to_active_hash :cost
+  belongs_to_active_hash :prefecture
+  belongs_to_active_hash :shipping
+
+  # belongs_to :user
+  # has_one :shipping
+  # has_one :item_purchase
 
   with_options presence: true do
     validates :name
-    validates :decription
+    validates :description
     validates :category_id
     validates :condition_id
     validates :cost_id
@@ -16,12 +20,6 @@ class Item < ApplicationRecord
     validates :shipping_id
     validates :price
   end
-
-  # validates :category_id, numericality: { other_than: 1 }
-  # validates :condition_id, numericality: { other_than: 1 }
-  # validates :cost_id, numericality: { other_than: 1 }
-  # validates :prefecture_id, numericality: { other_than: 1 }
-  # validates :shipping_id, numericality: { other_than: 1 }
 
   with_options numericality: { other_than: 1 } do
     validates :category_id
