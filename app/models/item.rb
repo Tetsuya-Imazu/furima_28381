998e2a0fda1a51e0fa83c775f793c0b9be2ec174
Tetsuya-Imazu <tty8131b@gmail.com>
2020-08-07 12:51:14 +0900

    itemsモデル単体テストコード実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -8,21 +8,17 @@ class Item < ApplicationRecord
   
   has_one_attached :image
 
-  # belongs_to :user
-  # has_one :shipping
-  # has_one :item_purchase
+  belongs_to :user
+  has_one :shipping
+  has_one :item_purchase
 
   with_options presence: true do
     validates :name
     validates :description
-    validates :category_id
-    validates :condition_id
-    validates :cost_id
-    validates :prefecture_id
-    validates :shipping_id
     validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
     validates :image
   end
+  
 
   with_options numericality: { other_than: 1 } do
     validates :category_id
