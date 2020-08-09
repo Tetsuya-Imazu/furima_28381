6459072257714c5ef57f3bd6be53fadfcd8e284b
Tetsuya-Imazu <tty8131b@gmail.com>
2020-08-09 18:24:36 +0900

    javascript記述

--- app/controllers/itempurchases_controller.rb
+++ app/controllers/itempurchases_controller.rb
@@ -1,9 +1,33 @@
 class ItempurchasesController < ApplicationController
+  before_action :authenticate_user!
+
   def index
     @item = Item.find(params[:item_id])
   end
 
   def create
+    @itempurchase = Itempurchase.new(itempurchase_params)
+    if @itempurchase.valid?
+      @pay_item
+      @itempurchase.save
+      redirect_to root_path
+    else
+      render 'index'
+    end
+  end
 
+  private
+
+  def itempurchase_params
+    params.permit(:post_code, :prefecture_id, :city, :address, :building, :phone_number, :item_id, :token).merge(user_id: current_user.id)
+  end
+
+  def pay_item
+    Payjp.api_key = "sk_test_0b20e238ad8014338394760b"
+    Payjp::Charge.create(
+     amount: @item.price,
+     card: itempurchase_params[:token],
+     currency:'jpy'
+   )
   end
 end
