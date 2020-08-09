c18472b32f7c390eadf444be6efa646e4418f7f1
Tetsuya-Imazu <tty8131b@gmail.com>
2020-08-09 18:27:08 +0900

    itempurchaseテーブルにカラム追加

--- db/migrate/20200808123749_create_destinations.rb
+++ db/migrate/20200808123749_create_destinations.rb
@@ -1,16 +1,13 @@
-class CreateItempurchases < ActiveRecord::Migration[6.0]
+class CreateDestinations < ActiveRecord::Migration[6.0]
   def change
-    create_table :itempurchases do |t|
-      t.references :user, null: false, foreign_key: true
-      t.references :item, null: false, foreign_key: true
-
+    create_table :destinations do |t|
       t.string :post_code, null: false
       t.integer :prefecture_id, null: false
       t.string :city, null: false
       t.string :address, null: false
       t.string :building
       t.string :phone_number, null: false
-      t.string :token, null: false
+      t.references :item, foreign_key: true
       t.timestamps
     end
   end
