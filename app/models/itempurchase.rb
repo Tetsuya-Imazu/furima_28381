23e9e30f6065b4f546f6e952580c6922e7683258
Tetsuya-Imazu <tty8131b@gmail.com>
2020-08-11 19:08:50 +0900

    テストコード実装

--- app/models/itempurchase.rb
+++ app/models/itempurchase.rb
@@ -1,4 +1,15 @@
 class Itempurchase < ApplicationRecord
   belongs_to :item
   belongs_to :user
+
+  with_options presence: true do
+    validates :post_code
+    validates :city
+    validates :address
+    validates :phone_number
+    validates :token
+  end
+
+  validates :prefecture_id, numericality: { other_than: 1 }
+
 end
--- spec/models/itempurchase_spec.rb
+++ spec/models/itempurchase_spec.rb
@@ -1,5 +1,49 @@
 require 'rails_helper'
 
 RSpec.describe Itempurchase, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '#create' do
+    before do
+      @itempurchase = FactoryBot.build(:itempurchase)
+    end
+
+    it '全ての項目が正しく入力されていれば保存できる' do
+      expect(@itempurchase).to be_valid
+    end
+
+    it 'post_codeが空だと保存できない' do
+      @itempurchase.post_code = nil
+      @itempurchase.valid?
+      expect(@itempurchase.errors.full_messages).to include("Post code can't be blank")
+    end
+
+    it 'prefecture_idが選択されていないと保存できない' do
+      @itempurchase.prefecture_id = nil
+      @itempurchase.valid?
+      expect(@itempurchase.errors.full_messages).to include("Prefecture is not a number")
+    end
+
+    it 'cityが空だと保存できない' do
+      @itempurchase.city = nil
+      @itempurchase.valid?
+      expect(@itempurchase.errors.full_messages).to include("City can't be blank")
+    end
+
+    it 'addressが空だと保存できない' do
+      @itempurchase.address = nil
+      @itempurchase.valid?
+      expect(@itempurchase.errors.full_messages).to include("Address can't be blank")
+    end
+
+    it 'phone_numberが空だと保存できない' do
+      @itempurchase.phone_number = nil
+      @itempurchase.valid?
+      expect(@itempurchase.errors.full_messages).to include("Phone number can't be blank")
+    end
+    
+    it 'tokenが空では保存できない' do
+      @itempurchase.token = nil
+      @itempurchase.valid?
+      expect(@itempurchase.errors.full_messages).to include("Token can't be blank")
+    end
+  end
 end
